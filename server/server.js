1d8fc12f7b7053d5a618a7070cdfa777eeacfb96
Carlie Smith <carliesmith011@gmail.com>
2022-04-26 20:48:19 -0400

    fixed most source code issues

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer }  = require('apollo-server-express');
 const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
@@ -7,13 +7,12 @@ const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
-const app = express();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
 
+async function startApolloServer (typeDefs, resolvers) {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  
+await server.start();
+const app = express();
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
@@ -34,5 +33,6 @@ db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
+  })
 });
+}
